cd082204a0782b9247fa75469b96df4b050d6b37
PelleSwelle <peterdonvang@protonmail.com>
2026-04-20 08:19:49 +0200

    getting flag dimensions from reference image

--- core/src/main/java/io/github/flags/AssemblyBoard.java
+++ core/src/main/java/io/github/flags/AssemblyBoard.java
@@ -1,20 +1,28 @@
 package io.github.flags;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class AssemblyBoard extends Actor {
     Flag flag;
-    float width;
-    float height;
+    Vector2 size;
 
     public AssemblyBoard(Flag flag) {
         this.flag = flag;
         // placeholder values
-        this.width = 900;
-        this.height = 474;
+        this.size = getDimensions(flag.country);
+    }
+
+    private Vector2 getDimensions(String countryName) {
+        Texture t = new Texture("flags/" + countryName + "/flag.png");
+        return new Vector2(
+            t.getWidth(),
+            t.getHeight()
+        );
     }
 
     @Override
@@ -26,9 +34,9 @@ public class AssemblyBoard extends Actor {
 
         float[] vertices = new float[] {
             getX(), getY(),
-            getX(), getY() + height,
-            getX() + width, getY() + height,
-            getX() + width, getY()
+            getX(), getY() + size.y,
+            getX() + size.x, getY() + size.y,
+            getX() + size.x, getY()
         };
         outlineRenderer.polygon(vertices);
         outlineRenderer.end();
